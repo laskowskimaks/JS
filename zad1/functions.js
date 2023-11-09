598fd87746d4ad8e4220e3875135c076732dcdf4
laskowskimaks <laskowski.maksymilian@gmail.com>
2023-11-09 09:14:53 +0100

    zrobione zad1

---
+++ zad1/functions.js
@@ -0,0 +1,19 @@
+function isOdd(x) {
+    if(x % 2 == 1)
+    {
+        return true;
+    }
+    return false;
+
+}
+console.log(isOdd(3));
+
+function isEven(x){
+        if(x % 2 == 0)
+        {
+            return true;
+        }
+        return false;
+}
+
+console.log(isEven(2));
