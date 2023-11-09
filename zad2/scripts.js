a4c8e2c57398a7f4a2c3675e7349d3e0d8b9f1c4
laskowskimaks <laskowski.maksymilian@gmail.com>
2023-11-09 10:25:58 +0100

    w trakcie robienia

---
+++ zad2/scripts.js
@@ -0,0 +1,8 @@
+const liczba = document.querySelector(h1.liczba);
+const plus1 = document.querySelector(.plus1);
+
+liczba.onclick('plus1', function(e) {
+    e.preventDefault();
+    liczba=liczba+1;
+    console.log(test);
+})
